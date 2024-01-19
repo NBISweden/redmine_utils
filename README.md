8131c5ad01d3bda5e40e8169f86c97473ba1aaa1
dahlo <m.dahlo@gmail.com>
2024-01-20 00:23:13 +0100

    Script is working, but in a debug state. Awaiting bug fixes in redmine plugin.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # redmine_utils
+
+2 problems:
+
+#### Error 500 when updating issues
+
+Doesn't work to update issues (like [6760](https://projects.nbis.se/issues/6760)), because the Redmine Surveys Notifier plugin crashes when there are `nil` values in the custom fields. No idea why my test issue did not have `nil` values, perhaps since i created it through the web ui and it assigns empty strings instead of `nil`. The other issues seem to be created by the forms or email? (Added by Support Request)
+
+A solution could be to add `.to_s` to the line in the plugin that crashes
+
+```ruby
+# in /data/redmine/redmine-3.4/plugins/redmine_surveys_notifier/app/models/surveys_notifier.rb, line 32
+
+::Rails.logger.info("custom field " + field.custom_field.name + " " + field.value.to_s)
+```
+
+
+#### Rejected is counted as closed
+
+The survey notifier plugin regards a status change to `Rejected` as the same as `Closed`, and thus triggers a survey email when a issue is rejected. I have not seen any survey email being sent out, so it is a bit unclear what happens after the plugin is triggered. The code looks like it should send it if it has triggered, but the mail server shows no email being sent. I can't find where the typeform it links to has been created, so i can't check if anyone has filled it in lately. If there are no responses, is it even being sent out? Hopefully someone should have noticed no responses coming in if that was the case?
+
+Could be solved by adding an exception for thie rejected status. 
+
+```ruby
+# in /data/redmine/redmine-3.4/plugins/redmine_surveys_notifier/app/models/surveys_notifier.rb, line 13
+
+if j.prop_key == "status_id" and issue.status.is_closed then
+
+# change to (according to chatgpt)
+if j.prop_key == "status_id" and issue.status.is_closed and issue.status.name != "Rejected" then
+```
--- resolve_old_lts_issues.py
+++ resolve_old_lts_issues.py
@@ -1,8 +1,13 @@
 import yaml
 import argparse
-from redminelib import Redmine
+import requests
+import pdb
+from pprint import pprint
 
 def main():
+    """
+    Main function to retrieve and update issues in Redmine.
+    """
     # Parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', help='Path to the YAML config file')
@@ -14,35 +19,142 @@ def main():
     with open(args.config, 'r') as config_file:
         config = yaml.safe_load(config_file)
 
-    # Connect to Redmine using API key
-    redmine = Redmine(config['url'], key=config['api_key'])
+    # Create Redmine API URL for projects
+    url = f"{config['url']}/projects.json"
+
+    # Set Redmine API key
+    headers = {"X-Redmine-API-Key": config['api_key']}
+
+    # Get all projects
+    projects = get_all_projects(url, headers)
 
     # Get the project named "Long-term Support"
     project_name = "Long-term Support"
-    project = redmine.project.get(project_name)
+    project_id = None
+    for project in projects:
+        if project['name'] == project_name:
+            project_id = project['id']
+
+    if project_id is None:
+        print(f"Project '{project_name}' not found.")
+        return
+
+    # Create Redmine API URL for issues
+    url = f"{config['url']}/issues.json"
+    update_issue_status(f"{config['url']}/issues", headers, 7312, 6)
+    import sys
+    sys.exit(1)
 
     # Get all issues in the project
-    issues = project.issues
+    issues = get_all_project_issues(url, headers, project_id)
 
     # Get the sub-project names to exclude
     exclude_projects = args.exclude.split(',') if args.exclude else []
 
+    # Set url for status update
+    url = f"{config['url']}/issues"
+
     # Iterate over the issues
     for issue in issues:
         # Check if the issue's custom field "WABI ID" is empty
-        if not issue.custom_fields.WABI_ID:
+        if not get_custom_field(issue, 'WABI ID'):
             # Exclude issues in the specified sub-projects
-            if issue.project.identifier not in exclude_projects:
+            if issue['project']['name'] not in exclude_projects:
                 # Print status message
-                print(f"Updating issue #{issue.id} - {issue.subject}")
-
+                print(f"Updating {issue['project']['name']} - issue #{issue['id']} - '{issue['subject']}'")
+            
+                pdb.set_trace()
                 # Perform dry run if specified
                 if args.dryrun:
                     continue
 
-                # Change the status of the issue to "Resolved"
-                issue.status_id = 5  # assuming "Resolved" status ID is 5
-                issue.save()
+                # Change the status of the issue to "Rejected"
+                update_issue_status(url, headers, int(issue['id']), 6)  # assuming "Rejected" status ID is 6
+
+def get_custom_field(issue, field_name):
+
+
+    # get field value
+    field_value = [ field['value'] for field in issue['custom_fields'] if field['name']==field_name ]
+    if len(field_value) > 0:
+        field_value = field_value[0]
+    else:
+        field_value = ''
+
+    return field_value
+
+def get_all_projects(url, headers):
+    """
+    Retrieve all projects from Redmine API by paginating through the results.
+    """
+    projects = []
+    page = 1
+    while True:
+        response = requests.get(url, headers=headers, params={"page": page})
+        response.raise_for_status()
+        data = response.json()
+        projects.extend(data['projects'])
+        if data['total_count'] <= len(projects):
+            break
+        page += 1
+    return projects
+
+def get_project_id(projects, project_name):
+    """
+    Retrieve the ID of a project from the given list of projects and its name.
+    """
+    for project in projects:
+        print(project['name'])
+        if project['name'] == project_name:
+            return project['id']
+    return None
+
+def get_all_project_issues(url, headers, project_id):
+    """
+    Retrieve all issues in a project from the Redmine API by paginating through the results.
+
+    Args:
+        url (str): The URL of the Redmine API.
+        headers (dict): The headers containing the API key for authentication.
+        project_id (int): The ID of the project.
+
+    Returns:
+        list: A list of all issues in the project.
+
+    """
+    issues = []
+    page = 1
+    while True:
+        # Retrieve issues for the current page
+        response = requests.get(url, headers=headers, params={"project_id": project_id, "page": page})
+        response.raise_for_status()
+        data = response.json()
+        issues.extend(data['issues'])
+
+        # Break the loop if all issues have been retrieved
+        if data['total_count'] <= len(issues):
+            break
+
+        # Move to the next page
+        page += 1
+
+    return issues
+
+def update_issue_status(url, headers, issue_id, status_id):
+    """
+    Update the status of an issue in Redmine using the provided URL, headers, issue ID, and status ID.
+    """
+    payload = {
+        "issue": {
+            "status_id": status_id,
+            "notes": "Cleaning out old issues."
+        },
+    }
+    issue_url = f"{url}/{issue_id}.json"
+    pdb.set_trace()
+    response = requests.put(issue_url, headers=headers, json=payload)
+    response.raise_for_status()
 
 if __name__ == '__main__':
+    # Run the main function
     main()
